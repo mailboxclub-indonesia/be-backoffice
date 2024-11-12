33d3961a19898a5b3426a60164b25db1e4cd797f
reizkian <reizkianyesaya@gmail.com>
2024-11-12 19:58:56 +0700

    Fix missing import

--- src/main/java/org/mailboxclubindonesia/backoffice/config/SecurityConfig.java
+++ src/main/java/org/mailboxclubindonesia/backoffice/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package org.mailboxclubindonesia.backoffice.config;
 
+import org.mailboxclubindonesia.backoffice.service.AuthenticationService;
+import org.mailboxclubindonesia.backoffice.filter.AuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
